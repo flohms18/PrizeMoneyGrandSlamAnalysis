eb7a2b3ff601bc3bc3ef84b2367da1598e248eeb
flotr18 <trouilletflorent@outlook.com>
2025-03-04 12:43:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('tennis_pay.csv').sort_values(by='year',ascending=False)
 df.iloc[::-1]
 
-GrandSlam = ['wimbledon_women','aus_open_women','us_open_women','roland_garros_women']
+GrandSlam_Women = ['wimbledon_women','aus_open_women','us_open_women','roland_garros_women']
+GrandSlam_Men = ['wimbledon_men','aus_open_men','us_open_men','roland_garros_men']
 
-df.plot(kind='line',x='year',y=GrandSlam,legend=False)
+df.plot(kind='line',x='year',y=GrandSlam_Men,legend=False)
 
 plt.show()
