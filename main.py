0df8b69ea280c4485fa3845375644499658efe81
flotr18 <trouilletflorent@outlook.com>
2025-03-04 14:44:11 +0100

    Evolution of PZM for men' grand slams

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('tennis_pay.csv')
 print(df.head())
+
+GrandSlam = ['wimbledon_men','us_open_men','aus_open_men','roland_garros_men']
+
+men_colors = ['#77C264','#1DAFDB','#4291CF','#D85428']
+
+df.plot(kind='line',x='year',y=GrandSlam,color=men_colors)
+
+plt.show()
