d8cb1c6c2ddb0e44d34522baefa128a6a853bf92
flotr18 <trouilletflorent@outlook.com>
2025-03-04 11:31:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import statistics as stat
 
 df = pd.read_csv('tennis_pay.csv').sort_values(by='year',ascending=False)
-df['year'] = df['year'].astype(int)
 
-df.plot(kind='line',x='year',y=['wimbledon_women','wimbledon_men'],legend=False)
-
-plt.show()
+for index, row in df.iterrows():
+    print(row['year'])
