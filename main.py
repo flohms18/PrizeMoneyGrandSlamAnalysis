156ec8133473c3ece0ec5d3c20e4ab073fdc8c57
flotr18 <trouilletflorent@outlook.com>
2025-03-04 10:24:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('tennis_pay.csv')
 
 for i in df['year']:
-    print(f"in {i}, the prize money was equal to {df['wimbledon_women']}")
+    print(f"in {i}, the prize money was equal to {df['year']}")
