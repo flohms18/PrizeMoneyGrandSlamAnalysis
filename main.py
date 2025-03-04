dd90d7200b6c2737db2079c24a55314b64db983a
flotr18 <trouilletflorent@outlook.com>
2025-03-04 13:27:04 +0100

    Add colors

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ df.iloc[::-1]
 
 GrandSlam_Women = ['wimbledon_women','aus_open_women','us_open_women','roland_garros_women']
 GrandSlam_Men = ['wimbledon_men','aus_open_men','us_open_men','roland_garros_men']
+colors = ['blue', 'red', 'orange', 'purple', 'green', 'yellow', 'brown', 'pink']
 
 GrandSlam = GrandSlam_Women + GrandSlam_Men
 print(GrandSlam)
 
-df.plot(x='year',y=GrandSlam,legend=False)
-
+df.plot(kind='line',x='year',y=GrandSlam,legend=False, color=colors)
 plt.show()
+
+
+
